ae1ae8dd404a008967362888f4b0ee58a92f0d44
mohammad-anash <anash40678910@gmail.com>
2023-05-08 12:39:09 +0530

    changes

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
 let bodyel = document.querySelector('body');
 
-let letter = "123456789ABCDEF"
+let letter = "123456789ABCDEF";
 
-
-function randomcircle() {
-    let hash = "#";
+function randomcolor() {
+    let hash = '#';
     for (let index = 0; index < 6; index++) {
-       hash += letter[Math.floor(Math.random()*letter.length)];
+        hash += letter[Math.floor(Math.random() * letter.length)];
     }
-    console.log(hash)
-   return hash
+    return hash
 }
 
 
-function getrandomsize(min, max){
-    return Math.random() * (max - min) + min;
-}
 
-function createcircle(event) {
+function createcircles() {
     let div = document.createElement('div');
     div.classList.add('circle');
-    let xpos = event.clientX;
-    let ypos = event.ClientY;
-    let size = getrandomsize(10, 200)
-    div.style.width = `${size}`;
-    div.style.height = `${size}`;
-    div.style.left = `${xpos}px`;
-    div.style.top = `${ypos}px`;
-    div.style.backgroundColor = randomcircle();
-    console.log(div)
-    bodyel.appendChild(div)
-    return div
+    div.style.backgroundColor = randomcolor()
+    bodyel.appendChild(div);
 }
 
+let currentcircle = createcircles();
+
+document.body.appendChild(currentcircle)
 
 
-document.addEventListener('click', (event) => {
-    createcircle(event);
-    console.log('button was clicked')
+document.addEventListener('click', (e) => {
+    currentcircle = createcircles()
+
+    currentcircle.style.top = `${e.clientX - currentcircle.offsetWidth / 2}px`
+    currentcircle.style.left = `${e.clientY - currentcircle.offsetHeight / 2}px`
 })
+
+function newFunction() {
+    console.log(currentcircle);
+}
